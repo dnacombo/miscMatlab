8174f53c93bc6e48f4b0e0f1071a4b306f72de70
dnacombo <maximilien.chaumon@gmail.com>
2016-07-07 09:03:04 +0200

    add a little help

--- num2cellstr.m
+++ num2cellstr.m
@@ -1,6 +1,9 @@
-function c = num2cellstr(num)
-
+function c = num2cellstr(num,fmt)
+% c = num2cellstr(num,fmt)
+% creates a cell array of strings containing numel(num) strings using
+% numstr(num) using format string fmt
+%
 c = cell(size(num));
 for i = 1:numel(num)
-    c{i} = num2str(num(i));
+    c{i} = num2str(num(i),fmt);
 end
