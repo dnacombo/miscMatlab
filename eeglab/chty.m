f9834353bbbb43bdb9702809edd0a879a98f8d5a
dnacombo <chaumonm@Solaris.mnb>
2016-07-14 10:05:35 +0200

    "add"

---
+++ eeglab/chty.m
@@ -0,0 +1,58 @@
+function [nb,channame,strnames] = chty(channame, varargin)
+
+% chnb() - return channel number corresponding to channel names in an EEG
+%           structure
+%
+% Usage:
+%   >> [nb]                 = chnb(channameornb);
+%   >> [nb,names]           = chnb(channameornb,...);
+%   >> [nb,names,strnames]  = chnb(channameornb,...);
+%   >> [nb]                 = chnb(channameornb, type);
+%
+% Input:
+%   channameornb  - If a string or cell array of strings, it is assumed to
+%                   be (part of) the name of channels to search. Either a
+%                   string with space separated channel names, or a cell
+%                   array of strings. 
+%                   Note that regular expressions can be used to match
+%                   several channels. See regexp.
+%                   If only one channame pattern is given and the string
+%                   'inv' is attached to it, the channels NOT matching the
+%                   pattern are returned.
+%   types         - Channel types as found in {EEG.chanlocs.type}.
+%
+% Output:
+%   nb            - Channel numbers in labels, or in the EEG structure
+%                   found in the caller workspace (i.e. where the function
+%                   is called from) or in the base workspace, if no EEG
+%                   structure exists in the caller workspace.
+%   names         - Channel names, cell array of strings.
+%   strnames      - Channel names, one line character array.
+
+error(nargchk(1,2,nargin));
+if nargin == 2
+    if isstruct(varargin{1}) && isfield(varargin{1},'setname')
+        % assume it's an EEG dataset
+        types = {varargin{1}.chanlocs.type};
+    else
+        types = varargin{1};
+    end
+else
+    
+    try
+        EEG = evalin('caller','EEG');
+    catch
+        try
+            EEG = evalin('base','EEG');
+        catch
+            error('Could not find EEG structure');
+        end
+    end
+    if not(isfield(EEG,'chanlocs'))
+        error('No channel list found');
+    end
+    EEG = EEG(1);
+    types = {EEG.chanlocs.type};
+end
+
+[nb,channame,strnames] = chnb(channame, types);
