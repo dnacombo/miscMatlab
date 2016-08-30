740dc1774a17cee1b646cb29426995e424db46bd
dnacombo <chaumonm@Solaris.mnb>
2016-08-30 11:08:14 +0200

    "add rejE for electrode rejections"

--- eeglab/pop_addrej.m
+++ eeglab/pop_addrej.m
@@ -1,19 +1,23 @@
-function EEG = pop_addrej(EEG,name,rej,col)
+function EEG = pop_addrej(EEG,name,rej,rejE,col)
 
-% EEG = pop_addrej(EEG,name,rej,col)
+% EEG = pop_addrej(EEG,name,rej[,rejE],col)
 %
-% add the rejections in rej to the EEG structure and prepare to display
-% them with color col (RGB) with pop_eegplot
+% add the rejections in rej (and rejE) to the EEG structure and prepare to
+% display them with color col (RGB) with pop_eegplot
 %
 %
-if ~islogical(rej) && all(ismember(unique(rej) ,[0 1]))
+if ~islogical(rej) && all(ismember(unique(rej) ,[0 1])) && isequal(size(rej),[1,EEG.nbchan])
     rej = logical(rej);
+end
+if nargin == 4 && numel(rejE) == 3 % assume fourth input is col
+    col = rejE;
+    rejE = zeros(EEG.nbchan,EEG.trials);
 end
 
 
 EEG.reject.(['rej' name]) = zeros(1,EEG.trials);
 EEG.reject.(['rej' name])(rej) = 1;
-EEG.reject.(['rej' name 'E']) = zeros(EEG.nbchan,EEG.trials);
+EEG.reject.(['rej' name 'E']) = rejE;
 EEG.reject.(['rej' name 'col']) = col;
 
 EEG.reject.disprej(strcmp(EEG.reject.disprej,name)) = [];
