738f5165c8b1ce00bf6813d26fad5abe215da2f4
dnacombo <chaumonm@Solaris.mnb>
2016-09-01 12:23:40 +0200

    "Fix a bug that made chanlocs(chan2) disappear"

--- eeglab/pop_chanswap.m
+++ eeglab/pop_chanswap.m
@@ -8,13 +8,14 @@ function [EEG,com] = pop_chanswap(EEG,chan1,chan2)
 if isempty(chnb(chan1)) || isempty(chnb(chan2))
     error('channel not found')
 end
-
+chan1 = chnb(chan1);
+chan2 = chnb(chan2);
 d = EEG.data(chnb(chan1),:);
-EEG.data(chnb(chan1),:) = EEG.data(chnb(chan2),:);
-EEG.data(chnb(chan2),:) = d;
+EEG.data(chan1,:) = EEG.data(chan2,:);
+EEG.data(chan2,:) = d;
 c = EEG.chanlocs(chnb(chan1));
-EEG.chanlocs(chnb(chan1)) = EEG.chanlocs(chnb(chan2));
-EEG.chanlocs(chnb(chan2)) = c;
+EEG.chanlocs(chan1) = EEG.chanlocs(chan2);
+EEG.chanlocs(chan2) = c;
 
 com = sprintf('EEG = pop_chanswap( %s,%s);', inputname(1), vararg2str({chan1, chan2}));
 
