eb957bc8c77687defc0077124d9a1dba5333153d
dnacombo <chaumonm@Solaris.mnb>
2016-09-07 17:55:39 +0200

    "add sem.m"

---
+++ sem.m
@@ -0,0 +1,9 @@
+function s = sem(data,dim)
+
+% s = sem(data,dim)
+%
+% compute standard error of the mean of data along dimension dim
+% 
+% s = mean(data,dim)/sqrt(std(data,[],dim));
+
+s = mean(data,dim)/sqrt(std(data,[],dim));
