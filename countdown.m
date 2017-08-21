ea3c96f8e3f7e298bd307b8664fdfa8d9b7a073f
Maximilien Chaumon <maximilien.chaumon@gmail.com>
2017-08-21 14:31:38 +0200

    add a step argument

--- countdown.m
+++ countdown.m
@@ -1,13 +1,15 @@
-function countdown(seconds)
+function countdown(seconds,step)
 
 % countdown(seconds)
 % display a count down of seconds in the command window
-
+if not(exist('step','var'))
+    step = .01;
+end
 tic
 n = 0;
 try
     while toc < seconds
-        pause(.01)
+        pause(step)
         fprintf(repmat('\b',1,n))
         n = fprintf('%g',round(seconds - toc));
     end
