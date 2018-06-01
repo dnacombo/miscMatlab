2f48d93339d238924969c3a786e542ec31d44d65
maximilien.chaumon <maximilien.chaumon@icm-institute.org>
2018-06-01 15:44:53 +0200

    ft_powspec : add support for multiple data input

--- fieldtrip/ft_powspec.m
+++ fieldtrip/ft_powspec.m
@@ -1,5 +1,8 @@
 function spectrum = ft_powspec(cfg,data)
 
+if not(iscell(data))
+    data = {data};
+end
 
 def.channel                 = {'all'};
 def.method                  = 'mtmfft';
@@ -8,13 +11,14 @@ def.foilim                  = [0 110];
 def.taper                   = 'hanning';
 
 cfg = setdef(cfg,def);
-
-spectrum                    = ft_freqanalysis(cfg,data);
-
-
-
 figure;
 
-loglog(spectrum.freq,spectrum.powspctrm(:,:)')
+spectrum = cell(size(data));
+for i_dat = 1:numel(data)
+    spectrum{i_dat}                    = ft_freqanalysis(cfg,data{i_dat});
+    loglog(spectrum{i_dat}.freq,spectrum{i_dat}.powspctrm(:,:)');
+    hold on
+end
 xlabel('Frequency')
 ylabel('Power')
+xlim(minmax(spectrum{i_dat}.freq))
