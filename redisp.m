14e0ac669d0ff53eaf6c2a202323364ba4f845cf
maximilien.chaumon <maximilien.chaumon@icm-institute.org>
2018-06-01 15:43:06 +0200

    redisp : add support for numeric input

--- redisp.m
+++ redisp.m
@@ -6,7 +6,9 @@ persistent nRedisp
 if isempty(nRedisp)
     nRedisp = 0;
 end
-
+if isnumeric(txt)
+    txt = num2str(txt);
+end
 if exist('txt','var')
     fprintf(repmat('\b',1,nRedisp));
     nRedisp = fprintf(txt);
