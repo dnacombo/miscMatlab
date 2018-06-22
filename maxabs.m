5f649dd26bf66b2eaaf0ab7a25a0ad31ddfc5e4e
maximilien.chaumon <maximilien.chaumon@icm-institute.org>
2018-06-22 14:22:51 +0200

    maxabs: add returning index of max

--- maxabs.m
+++ maxabs.m
@@ -1,14 +1,16 @@
-function m = maxabs(a,opt)
-% m = maxabs(a)
-% return the maximum of the absolute value of a;
-% m = maxabs(a,'+-')
-% return two values (a negative and a positive)
+function [m,i] = maxabs(a,opt)
+% [m,i] = maxabs(a)
+% return m: the maximum of the absolute value of a;
+%        i: the index of maxabs(a) in a
+% 
+% m = maxabs(a,'-+')
+% return m a vector with two values (a negative and a positive)
 % this is useful to scale images.
 
 if ~exist('opt','var') || isempty(opt)
     opt = '';
 end
-m = max(abs(a(:)));
+[m, i] = max(abs(a(:)));
 if not(isempty(strfind(opt,'+-'))) || not(isempty(strfind(opt,'-+')))
     m = [-m m];
 end
