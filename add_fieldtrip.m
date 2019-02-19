f288581e08c4a281a3cd3a5af2253e46ede74728
Maximilien Chaumon <maximilien.chaumon@icm-institute.org>
2019-02-19 11:52:42 +0100

    add_fieldtrip

---
+++ add_fieldtrip.m
@@ -0,0 +1,17 @@
+function add_fieldtrip
+
+% add_fieldtrip
+% 
+
+% in case I used eeglab recently, avoid conflicts
+rm_frompath('eeglab.*Fieldtrip')
+
+% my fieldtrip is stored under my userpath
+addpath(fullfile(userpath, 'fieldtrip'));
+
+% for cleaner output
+global ft_default
+ft_default.showcallinfo = 'no';
+
+ft_defaults
+
