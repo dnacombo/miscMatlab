7632226944d855c9178273a71f555ce98e3b2ea0
Maximilien Chaumon <maximilien.chaumon@icm-institute.org>
2019-09-12 09:49:09 +0200

    ft_excludebad: crop events at beginning or end of recording

--- fieldtrip/ft_excludebad.m
+++ fieldtrip/ft_excludebad.m
@@ -42,12 +42,38 @@ toexclude = event(strcmp({event.type},cfg.excludename));
 if isempty(toexclude)
     warning(['No ' cfg.excludename ' marker found.'])
 end
+% now crop events at the beginning or end of the recording
+todel = [];
+for i = 1:numel(event)
+    % set negative samples to 0
+    if event(i).offset < 0
+        if event(i).duration < -event(i).offset
+            % plan to remove if both beginning and end are before data
+            todel(end+1) = i;
+            continue
+        end
+        event(i).duration = event(i).duration + event(i).offset + 1;
+        event(i).offset = 0;
+        event(i).sample = 1;
+    end
+    if event(i).sample + event(i).duration -1 > cfgmrk.hdr.nSamples
+        if event(i).sample > cfgmrk.hdr.nSamples
+            todel(end+1) = i;
+        end
+        event(i).duration = cfgmrk.hdr.nSamples - event(i).sample;
+    end
+end
+event(todel) = [];
+
 cfgbad = [];
 cfgbad.trl = [];
 cfgbad.trl(:,1) = [1 [event.sample] + [event.duration]+1]';
 cfgbad.trl(:,2) = [[event.sample]-1 cfg.hdr.nSamples]';
 cfgbad.trl(:,3) = 0;
+% remove exclude after end of data
 cfgbad.trl(cfgbad.trl(:,1)>=numel(data.time{1}),:) = [];
+% remove exclude before beginning of data
+cfgbad.trl(cfgbad.trl(:,2)<=1,:) = [];
 
 data = ft_redefinetrial(cfgbad,data);
 
