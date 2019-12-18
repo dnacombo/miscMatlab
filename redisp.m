f53f0c8b8b5a85abf05fabd0283705ebb40798aa
Maximilien Chaumon <maximilien.chaumon@icm-institute.org>
2019-12-18 16:14:04 +0100

    redisp clean
    # Conflicts:
    #	redisp.m

--- redisp.m
+++ redisp.m
@@ -6,12 +6,18 @@ persistent nRedisp
 if isempty(nRedisp)
     nRedisp = 0;
 end
+if not(exist('txt','var'))
+    nRedisp = 0;
+    return
+end
+
+if isnumeric(txt)
+    txt = num2str(txt);
+end
 if exist('txt','var')
     if isnumeric(txt)
         txt = num2str(txt);
     end
     fprintf(repmat('\b',1,nRedisp));
     nRedisp = fprintf(txt);
-else
-    nRedisp = 0;
 end
