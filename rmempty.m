e2a82b3d9802bbd3b0c378fafe38cd9301aacc47
Maximilien Chaumon <maximilien.chaumon@icm-institute.org>
2019-12-18 16:17:12 +0100

    rmempty more general

--- rmempty.m
+++ rmempty.m
@@ -1,6 +1,14 @@
 function [in,todel] = rmempty(in)
 % [out] = rmempty(in)
 % remove empty cells from a cellarray
-todel = find(emptycells(in));
+todel = [];
+try
+    todel = find(emptycells(in));
+catch
+    for i = 1:numel(in)
+        if isempty(in(i))
+            todel(end+1) = i;
+        end
+    end
+end
 in(todel) = [];
-        
