7d564c24ebc5e8023b13014bc1e4bc32cc48efa7
Maximilien Chaumon <maximilien.chaumon@icm-institute.org>
2019-12-18 16:16:50 +0100

    redisp cosmit

--- redisp.m
+++ redisp.m
@@ -6,10 +6,10 @@ persistent nRedisp
 if isempty(nRedisp)
     nRedisp = 0;
 end
-if isnumeric(txt)
-    txt = num2str(txt);
-end
 if exist('txt','var')
+    if isnumeric(txt)
+        txt = num2str(txt);
+    end
     fprintf(repmat('\b',1,nRedisp));
     nRedisp = fprintf(txt);
 else
