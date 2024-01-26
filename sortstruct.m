7e0970662f1f91f678cbc6b82c980d3d7d3af278
Maximilien Chaumon <maximilien.chaumon@icm-institute.org>
2024-01-26 16:02:50 +0100

    strange edit to sortstruct.m

--- sortstruct.m
+++ sortstruct.m
@@ -19,13 +19,18 @@ if nargin < 2
     by = fieldnames(S);
     warning('sortstruct:sortallfields','no sorting fields provided. sorting by all of them...')
 end
-defifnotexist('direction','ascend');
 if ischar(by)
     defifnotexist('sorter',[]);
+    defifnotexist('direction','ascend');
     by = {by};
     sorter = {sorter};
+    direction = {direction};
 else
     defifnotexist('sorter',repmat({[]},1,numel(by)));
+    if not(exist('direction','var'))
+        direction = 'ascend';
+    end
+    defifnotexist('direction',repmat({direction},1,numel(by)));
 end
 if all(cellfun(@isempty,by))
     S = reshape(S,s);
@@ -34,24 +39,30 @@ end
 if ischar(S(1).(by{1}))
     L = {S.(by{1})};% list all values
     U = unique(L);% unique values (sorted)
-    U = [sorter{1} setxor(sorter{1},U)];
+    if strcmp(direction{1},'descend')
+        U = U(end:-1:1);
+    end
+    U = [sorter{1} setxor(sorter{1},U, 'stable')];
     nS = [];
     for i_u = 1:numel(U)% for each unique value
         tmpS = S(strcmp(L,U{i_u}));% extract them
         if numel(by) > 1
-            tmpS = sortstruct(tmpS,by(2:end),direction,sorter(2:end));% sort under
+            tmpS = sortstruct(tmpS,by(2:end),direction(2:end),sorter(2:end));% sort under
         end
         nS = [nS; tmpS];
     end
 elseif isnumeric(S(1).(by{1}))
     L = [S.(by{1})];% list all values
     U = unique(L);% unique values (sorted)
-    U = [sorter{1} setxor(sorter{1},U)];
+    if strcmp(direction{1},'descend')
+        U = U(end:-1:1);
+    end
+    U = [sorter{1} setxor(sorter{1},U, 'stable')];
     nS = [];
     for i_u = 1:numel(U)% for each unique value
         tmpS = S(L == U(i_u));% extract them
         if numel(by) > 1
-            tmpS = sortstruct(tmpS,by(2:end),direction,sorter(2:end));% sort under
+            tmpS = sortstruct(tmpS,by(2:end),direction(2:end),sorter(2:end));% sort under
         end
         nS = [nS; tmpS];
     end
